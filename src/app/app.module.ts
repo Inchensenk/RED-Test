da67ed6fca25bc4c98cee4f3b75921e89270fcc4
Inchensenk <aab8117@gmail.com>
2023-06-26 11:26:54 +0300

    ...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { LayoutComponent } from './components/ui/layout/layout.component';
+import { HeaderComponent } from './components/ui/layout/header/header.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
